0220cc4d5c742ba673a6bc34f33a26d9dd72d833
subash <subas@vayu.com.au>
2017-08-14 08:04:49 +0545

    Add edit show update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,22 +1,50 @@
 class ArticlesController < ApplicationController
 
+
+  def index
+    @articles = Article.all
+
+  end
   def new
-      @article = Article.new
-      @description = Article.new
+    
+  @article = Article.new
+
   end
 
-
+  def edit
+    @article = Article.find(params[:id])
+  end
 
   def create
-    # render plain: params[:article].inspect
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+    if @article.save
+      flash[:notice] = "Article was sucessfully created"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was sucessfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 
   private
-    def article_params
-      params.require(:article).permit(:title, :description)
-    end
+
+  def article_params
+
+    params.require(:article).permit(:title, :description)
+
+  end
 
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
 
   def home
+    # byebug
   end
 
   def about
